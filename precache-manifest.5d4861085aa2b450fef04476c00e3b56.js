47742fa5dc1cf65bab304e75ada83c01e6a7c95b
alu@win8.1-asus-pc <dev.bhorta@gmail.com>
2020-11-03 17:15:24 +0600

    Updates

---
+++ precache-manifest.5d4861085aa2b450fef04476c00e3b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38373b996f8eb20cac084e9ec300f116",
+    "url": "/cactus-frontend-assignment/index.html"
+  },
+  {
+    "revision": "c34764481b0d3fb9dc51",
+    "url": "/cactus-frontend-assignment/static/css/main.5779ac26.chunk.css"
+  },
+  {
+    "revision": "85a957be2b165c08fcca",
+    "url": "/cactus-frontend-assignment/static/js/2.7dac0886.chunk.js"
+  },
+  {
+    "revision": "c5e0836f4fec96a23f15c31d4f7ac8f0",
+    "url": "/cactus-frontend-assignment/static/js/2.7dac0886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34764481b0d3fb9dc51",
+    "url": "/cactus-frontend-assignment/static/js/main.a31cf070.chunk.js"
+  },
+  {
+    "revision": "4af1e34e9082eab024ce",
+    "url": "/cactus-frontend-assignment/static/js/runtime-main.8f1dd839.js"
+  }
+]);
